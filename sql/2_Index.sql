6a84827283cdca5bc0c6ee70cd431a0e462d72a4
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-10-08 16:55:16 +0900

    index?

--- sql/2_Index.sql
+++ sql/2_Index.sql
@@ -1,1 +1,2 @@
-ALTER TABLE isu_condition ADD INDEX uuid_timestamp (jia_isu_uuid, timestamp DESC);
+ALTER TABLE isu_condition ADD INDEX uuid_timestamp_idx (jia_isu_uuid, timestamp DESC);
+ALTER TABLE isu_condition ADD INDEX uuid_idx (jia_isu_uuid);
