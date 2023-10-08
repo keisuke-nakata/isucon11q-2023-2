678c9a784a7fba13fa3321cc678ec0362d92c118
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-10-08 12:09:21 +0900

    a

--- snapshot/init.sh
+++ snapshot/init.sh
@@ -3,38 +3,43 @@ source "$(dirname "$0")/config.sh"
 set -eux
 
 # result
-# git branch ${RESULT_BRANCH}
-# git checkout ${RESULT_BRANCH}
-# mkdir -p ${RESULT_BASE_DIR}
-# echo -e "|dt|score|commit id|change log|\n|--|--|--|--|" > ${RESULT_BASE_DIR}/summary.md
-# git add ${REPO_ROOT_DIR}
-# git commit -m "summary.md"
-# git push origin ${RESULT_BRANCH}
+git branch ${RESULT_BRANCH}
+git checkout ${RESULT_BRANCH}
+mkdir -p ${RESULT_BASE_DIR}
+echo -e "|dt|score|commit id|change log|\n|--|--|--|--|" > ${RESULT_BASE_DIR}/summary.md
+git add ${REPO_ROOT_DIR}
+git commit -m "summary.md"
+git push origin ${RESULT_BRANCH}
 
-readonly cmd="cd ${REPO_ROOT_DIR} && git fetch origin && git checkout -b result --track origin/result"
+cmd="cd ${REPO_ROOT_DIR} && git fetch origin && git checkout -b result --track origin/result"
 $SSH $APPSERVER2_PRIVATE_IP $cmd
 $SSH $APPSERVER3_PRIVATE_IP $cmd
 
 # main
-# git checkout ${RELEASE_BRANCH}
-# git pull origin ${RELEASE_BRANCH}
+git checkout ${RELEASE_BRANCH}
+git pull origin ${RELEASE_BRANCH}
 
-# mkdir -p ${CONF_DIR}
-# mkdir -p ${CONF_DIR}/sql
-# touch ${CONF_DIR}/sql/.gitkeep
-# mkdir -p ${CONF_DIR}/nginx
-# touch ${CONF_DIR}/nginx/.gitkeep
-# mkdir -p ${CONF_DIR}/memcached
-# touch ${CONF_DIR}/memcached/.gitkeep
+mkdir -p ${CONF_DIR}
+mkdir -p ${CONF_DIR}/sql
+touch ${CONF_DIR}/sql/.gitkeep
+mkdir -p ${CONF_DIR}/nginx
+touch ${CONF_DIR}/nginx/.gitkeep
+mkdir -p ${CONF_DIR}/memcached
+touch ${CONF_DIR}/memcached/.gitkeep
+# pprof
+mkdir -p ${PPORF_DIR}  # これは git 管理しないので .gitkeep 不要
+(
+    cd $REPO_ROOT_DIR/webapp/go
+    /home/isucon/local/go/bin/go get github.com/pkg/profile
+    /home/isucon/local/go/bin/go build -o isucondition
+)
+sudo systemctl restart isucondition.go
 
-# cp ${MYSQL_CONF_DEST} ${MYSQL_CONF_SRC}
-# cp ${NGINX_ROOT_CONF_DEST} ${NGINX_ROOT_CONF_SRC}
-# cp ${NGINX_SITE_CONF_DEST} ${NGINX_SITE_CONF_SRC}
-# # cp ${MEMCACHED_CONF_DEST} ${MEMCACHED_CONF_SRC}
+cp ${MYSQL_CONF_DEST} ${MYSQL_CONF_SRC}
+cp ${NGINX_ROOT_CONF_DEST} ${NGINX_ROOT_CONF_SRC}
+cp ${NGINX_SITE_CONF_DEST} ${NGINX_SITE_CONF_SRC}
+# cp ${MEMCACHED_CONF_DEST} ${MEMCACHED_CONF_SRC}
 
-# git add ${REPO_ROOT_DIR}
-# git commit -m "conf"
-# git push origin ${RELEASE_BRANCH}
-
-# # pprof
-# mkdir -p ${PPORF_DIR}
+git add ${REPO_ROOT_DIR}
+git commit -m "conf"
+git push origin ${RELEASE_BRANCH}
